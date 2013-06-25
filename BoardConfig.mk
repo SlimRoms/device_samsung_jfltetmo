360e4633b156e0a0efec9416024ef92376ab6b06
itsmikeramsay <ronaldramsayii@gmail.com>
2013-06-25 16:05:21 -0400

    Illusion ready... CM's kernel defconfig...
    Change-Id: Ia89ead44c8da7f64ad7a87847ee2f5228002e0e0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,4 +30,4 @@
 TARGET_OTA_ASSERT_DEVICE := jfltetmo
 
 # Kernel
-TARGET_KERNEL_CONFIG         := jf_can_defconfig
+TARGET_KERNEL_CONFIG         := jf_tmo_defconfig
--- illusion.mk
+++ illusion.mk
@@ -3,28 +3,28 @@ $(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
 # Release name
 PRODUCT_RELEASE_NAME := jfltetmo
 
-# Inherit some common SLIM stuff.
-$(call inherit-product, vendor/slim/config/gsm.mk)
+# Inherit some common illusion stuff.
+$(call inherit-product, vendor/illusion/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/illusion/config/nfc_enhanced.mk)
 
-# Inherit some common SLIM stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit some common illusion stuff.
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
 
 
 # Inherit device settings
-$(call inherit-product, vendor/slim/config/common_sgs.mk)
+$(call inherit-product, vendor/illusion/config/common_sgs.mk)
 
 # Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+$(call inherit-product, vendor/illusion/config/common_ledflash.mk)
 
 PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+    vendor/illusion/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
 
-PRODUCT_NAME := slim_jfltetmo
+PRODUCT_NAME := illusion_jfltetmo
 PRODUCT_DEVICE := jfltetmo
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SGH-M919
