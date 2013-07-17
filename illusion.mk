1d5749db649e81102ce7071607dbcd58e5931159
Mike Ramsay <ronaldramsayii@gmail.com>
2013-07-17 01:28:48 -0400

    Update illusion.mk

--- illusion.mk
+++ illusion.mk
@@ -22,16 +22,11 @@ $(call inherit-product, vendor/illusion/config/common_ledflash.mk)
 PRODUCT_COPY_FILES +=  \
     vendor/illusion/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE := jfltetmo \
-    PRODUCT_BRAND := samsung \
-    PRODUCT_MODEL := SGH-M919 \
-    PRODUCT_MANUFACTURER := Samsung \
-    BUILD_NUMBER=M919UVUAMDB \
-    PRODUCT_NAME=illusion_jfltetmo \
-    TARGET_DEVICE=jfltetmo \
-    TARGET_BUILD_TYPE=user \
-    BUILD_VERSION_TAGS=release-keys \
-    PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys" \
-    BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
+
+PRODUCT_NAME := illusion_jfltetmo
+PRODUCT_DEVICE := jfltetmo
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SGH-M919
+PRODUCT_MANUFACTURER := Samsung
 
